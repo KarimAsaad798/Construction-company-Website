39e03a2789b78d7fa3a204acc710fceb705b5315
Karim Asaad <KarimAsaad798/github.com>
2021-12-31 15:56:45 +0200

    files uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ClientsComponent } from './clients/clients.component';
+import { CONTACTComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ServicesComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ClientsComponent,
+    CONTACTComponent
   ],
   imports: [
     BrowserModule
